804de5f9a1e6b89e1a7dd8db5e2a3ff171ca2dde
Tobias Staudt <tobias.staudt@adam-riese.de>
2023-04-07 17:09:08 +0200

    feat(example): removed unnecessary template contents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Actions, useDirectiveEffects} from '@ngneat/effects-ng';
+import {test1, Test1Effect} from 'src/app/test.effect';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    hostDirectives: [useDirectiveEffects(Test1Effect)]
 })
 export class AppComponent {
-  title = 'effects-ng-directive';
+    title = 'effects-ng-directive';
+
+    constructor(private readonly actions: Actions) {
+    }
+
+    triggerAction(): void {
+        this.actions.dispatch(test1())
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,16 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {provideEffectsManager} from '@ngneat/effects-ng';
-import {TestComponent} from './test/test.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TestComponent
-  ],
-  imports: [
-    BrowserModule,
-  ],
-  providers: [provideEffectsManager()],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+    ],
+    providers: [provideEffectsManager()],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
